2c56d7c229377b8456a96c9d7bea5ed936daf776
Hans <hw.pers@hotmail.com>
2019-04-29 15:38:26 +0800

    feat(code): remove useless code

--- src/permission.js
+++ src/permission.js
@@ -1,18 +1,10 @@
 import router from './router'
 import store from './store'
-// import { Message } from 'element-ui'
 import NProgress from 'nprogress' // progress bar
 import 'nprogress/nprogress.css' // progress bar style
 import { getToken } from '@/utils/auth'
 
 NProgress.configure({ showSpinner: false }) // NProgress Configuration
-
-// permission judge function
-// function hasPermission(roles, permissionRoles) {
-//   if (roles.includes('admin')) return true // admin permission passed directly
-//   if (!permissionRoles) return true
-//   return roles.some(role => permissionRoles.indexOf(role) >= 0)
-// }
 
 const whiteList = ['/login', '/auth-redirect'] // no redirect whitelist
 router.beforeEach((to, from, next) => {
@@ -21,7 +13,7 @@ router.beforeEach((to, from, next) => {
     /* has token*/
     if (to.path === '/login') {
       next({ path: '/' })
-      NProgress.done() // if current page is dashboard will not trigger	afterEach hook, so manually handle it
+      NProgress.done()
     } else {
       // 判断当前用户是否已拉取完user_info信息
       if (!store.getters.user) {
--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -9,27 +9,18 @@ function mapAsyncRoutes(asyncRouterMap) {
     if (route.children) {
       route.children = mapAsyncRoutes(route.children)
     }
-    console.log('route', route)
     return route
   })
 }
 
 function generateRouter(item) {
-  // 表单 输入 path： /sys/menu
-  // name string-random 生成唯一字符串 xxxoo_sys_menu 形式 6个随机字符 + path 转换
-  // redirect： 判断如果是一级菜单 noredirect
-  // component: 判断如果是一级菜单component: Layout
   const meta = { icon: item.icon, title: item.name }
   var router = {
     path: item.url,
     name: item.alias,
     meta: meta,
-    // component: item.component === 'Layout' ? Layout : getViews(item.component),
     component: item.component === 'Layout' ? Layout : _import(item.component),
-    // redirect: item.component === 'Layout' ? 'noredirect' :  '',
-    // 面包屑上 点击 redirect 的 url  首页/系统管理/菜单管理  , 可点击系统管理
     redirect: item.redirect ? item.redirect : item.component === 'Layout' ? 'noredirect' : '',
-    // component: isParent ? Layout : componentsMap[item.name],
     alwaysShow: item.children.length === 1,
     children: item.children
   }
